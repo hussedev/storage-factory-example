02df62149f62e48af790cca039e5760e6b145aa7
hussedev <husse.dev@gmail.com>
2025-05-07 15:08:29 +0700

    create storage in a list

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -3,9 +3,9 @@ pragma solidity 0.8.18; // stating our version
 
 import {SimpleStorage} from "./SimpleStorage.sol";
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
     
     function create() public {
-        simpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(new SimpleStorage());
     }
 }
