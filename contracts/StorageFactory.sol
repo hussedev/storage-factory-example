763304d17dc7683f5ef5e618004f66413c6435f0
Huss Martinez <husse.dev@gmail.com>
2025-05-07 15:28:44 +0700

    simplified store/get functions

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -10,12 +10,10 @@ contract StorageFactory {
     }
 
     function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
-        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
-        simpleStorage.store(_newSimpleStorageNumber);
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256)  {
-       SimpleStorage simpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
-       return simpleStorage.retrieve();
+       return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
